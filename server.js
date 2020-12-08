1505f54313306c1685a3034d71a70a28a2ad218f
GustavoGomesDias <treeelevengg@gmail.com>
2020-12-08 20:15:52 -0300

    Add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 const mockUserData = [
     {name: 'Mark'},
     {name: 'Jill'}
@@ -21,6 +24,27 @@ app.get('/users/:id', function(req, res){
         message: 'Got one user. Nice!',
         users: req.params.id
     });
+});
+
+app.post('./login', function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if(username === mockUsername && password === mockPassword){
+        res.json({
+            success: true,
+            message: 'Password and username match!',
+            token: 'encrypted token goes here'
+        });
+    }else{
+        res.json({
+            success: false,
+            message: 'Password and username do not match!'
+        });
+    }    
 });
 
 app.listen(8000, function(){
